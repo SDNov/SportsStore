dd5161cfb277de16367082a3106cafd5380dd2b1
datchenko <sd_nov@mail.ru>
2016-06-25 15:11:46 +0300

    Add and register DI controller factory

---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    // Реализация пользовательской фабрики контроллеров,
+    // наследуясь от фабрики используемой по умолчанию
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            // получение объекта контроллера из контейнера
+            // используя его тип
+            return controllerType == null
+                ? null
+                : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            // конфигурирование контейнера
+            //throw new NotImplementedException();
+        }
+    }
+}
